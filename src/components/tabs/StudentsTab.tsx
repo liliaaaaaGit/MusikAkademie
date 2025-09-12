6bcc51ac6c1410bd71fbb373c886ff2bf495c1f3
liliaaaaaGit <lilia.schraut@icloud.com>
2025-09-12 20:39:50 +0200

    ui: simplify search placeholder text to 'Suche nach Schülern' on students page

--- src/components/tabs/StudentsTab.tsx
+++ src/components/tabs/StudentsTab.tsx
@@ -315,7 +315,7 @@ export function StudentsTab() {
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
                 <Input
-                  placeholder="Suchen nach Schülern, Instrumenten oder Lehrern..."
+                  placeholder="Suche nach Schülern"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
                   className="pl-10"
